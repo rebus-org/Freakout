6a202fde8dc26b59d3501df249cb5a566a2b4c06
Mogens Heller Grabe <mookid8000@gmail.com>
2024-05-20 18:56:07 +0200

    Update stuff and add more to this test

--- Freakout.Tests/Contracts/ExtensibilityTests.cs
+++ Freakout.Tests/Contracts/ExtensibilityTests.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Freakout.Internals;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Polly;
 using Polly.Retry;
 using Testy;
+using Testy.Extensions;
 using Testy.General;
 // ReSharper disable ClassNeverInstantiated.Local
+// ReSharper disable AccessToDisposedClosure
 
 namespace Freakout.Tests.Contracts;
 
@@ -26,23 +27,43 @@ public abstract class ExtensibilityTests<TFreakoutSystemFactory> : FixtureBase w
     [Test]
     public async Task WorksWithPolly()
     {
+        using var done = new ManualResetEvent(initialState: false);
+
         var stop = Using(new CancellationTokenSource());
 
-        Using(new DisposableCallback(() => stop.Cancel()));
+        Using(new DisposableCallback(stop.Cancel));
 
         var system = _factory.Create(after: services =>
         {
+            services.AddSingleton(done);
             services.Decorate<ICommandDispatcher, PollyCommandDispatcher>();
         });
 
         _ = system.StartCommandProcessorAsync(stop.Token);
 
+        using (system.CreateScope())
+        {
+            await system.Outbox.AddOutboxCommandAsync("HEJ", cancellationToken: stop.Token);
+        }
+
+        if (!done.WaitOne(TimeSpan.FromSeconds(5)))
+        {
+            throw new AssertionException("command was not handled within 5 s");
+        }
+    }
+
+    class ThrowingCommandHandler(ManualResetEvent done) : ICommandHandler<string>
+    {
+        public async Task HandleAsync(string command, CancellationToken cancellationToken)
+        {
+            done.Set();
+        }
     }
 
     class PollyCommandDispatcher(ICommandDispatcher commandDispatcher) : ICommandDispatcher
     {
-        static readonly AsyncRetryPolicy RetryPolicy = Policy.Handle<Exception>(e => e is not OperationCanceledException)
-            .WaitAndRetryAsync(10, n => TimeSpan.FromSeconds(n * 2));
+        readonly AsyncRetryPolicy RetryPolicy = Policy.Handle<Exception>(e => e is not OperationCanceledException)
+            .WaitAndRetryAsync(10, n => TimeSpan.FromMilliseconds(n * 2));
 
         public async Task ExecuteAsync(OutboxCommand outboxCommand, CancellationToken cancellationToken = default)
         {
